737775f5088b22ff9d0d7391dde36d2f4c48028a
Ackon Samuel <ackons433@gmail.com>
2022-12-10 12:43:07 +0000

    Add register and login functionality to students controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-    def register
-        user = User.create(user_params)
-        if user.valid? && user.save
-            render json: user,
-            status: 201
-            return
-        end
-        render json: user.errors,
-          status: 400
-    end
-
     private
 
     def user_params
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,33 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: %i[show update destroy]
 
+  # POST /student
+  def register
+    student = Student.create(student_params)
+    if student.valid? && student.save
+        render json: student,
+        status: 201
+        return
+    end
+    render json: student.errors,
+      status: 400
+end
+
+def login
+  email = params[:student][:email]
+  password = params[:student][:password]
+  student = Student.find_by(email: email)
+  is_valid = student && student.valid_password?(password)
+  unless is_valid
+      render json: {
+          status: 'error',
+          message: 'Invalid student credentials',
+      }, status: 400 and return
+  end
+  return render json: student,
+    status: 200
+end
+
   # GET /students
   def index
     @students = Student.all
