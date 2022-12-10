b7c2a2edd84c79d335fbd561f68fbf793c7e3f4f
Ackon Samuel <ackons433@gmail.com>
2022-12-10 13:18:25 +0000

    Add devise to clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Club < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :club_student
   has_many :club_staff
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :clubs
   devise_for :users
-  resources :clubs
-  resources :staffs
-  resources :students
+  # resources :clubs
+  # resources :staffs
+  # resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
