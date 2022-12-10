6acc0cb12cf0c97350b3a5b5c8a48f2e6883ae77
Ackon Samuel <ackons433@gmail.com>
2022-12-10 11:22:25 +0000

    Convert integer ids to uuids

--- db/migrate/20221210111616_devise_create_users.rb
+++ db/migrate/20221210111616_devise_create_users.rb
@@ -2,7 +2,8 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :users , id: :uuid do |t|
+      enable_extension 'pgcrypto'
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,73 +10,86 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_208_051_000) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_111616) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
-  create_table 'club_staffs', force: :cascade do |t|
-    t.bigint 'club_id', null: false
-    t.bigint 'staff_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['club_id'], name: 'index_club_staffs_on_club_id'
-    t.index ['staff_id'], name: 'index_club_staffs_on_staff_id'
+  create_table "club_staffs", force: :cascade do |t|
+    t.bigint "club_id", null: false
+    t.bigint "staff_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_id"], name: "index_club_staffs_on_club_id"
+    t.index ["staff_id"], name: "index_club_staffs_on_staff_id"
   end
 
-  create_table 'club_students', force: :cascade do |t|
-    t.bigint 'club_id', null: false
-    t.bigint 'student_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['club_id'], name: 'index_club_students_on_club_id'
-    t.index ['student_id'], name: 'index_club_students_on_student_id'
+  create_table "club_students", force: :cascade do |t|
+    t.bigint "club_id", null: false
+    t.bigint "student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_id"], name: "index_club_students_on_club_id"
+    t.index ["student_id"], name: "index_club_students_on_student_id"
   end
 
-  create_table 'clubs', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.integer 'telephone_number'
-    t.string 'group'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "clubs", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "telephone_number"
+    t.string "group"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'executives', force: :cascade do |t|
-    t.string 'portfolio'
-    t.bigint 'club_student_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['club_student_id'], name: 'index_executives_on_club_student_id'
+  create_table "executives", force: :cascade do |t|
+    t.string "portfolio"
+    t.bigint "club_student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_student_id"], name: "index_executives_on_club_student_id"
   end
 
-  create_table 'patrons', force: :cascade do |t|
-    t.bigint 'club_staff_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['club_staff_id'], name: 'index_patrons_on_club_staff_id'
+  create_table "patrons", force: :cascade do |t|
+    t.bigint "club_staff_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_staff_id"], name: "index_patrons_on_club_staff_id"
   end
 
-  create_table 'staffs', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.integer 'phone_number'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "staffs", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'students', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.integer 'phone_number'
-    t.string 'programme_name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "phone_number"
+    t.string "programme_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'club_staffs', 'clubs'
-  add_foreign_key 'club_staffs', 'staffs'
-  add_foreign_key 'club_students', 'clubs'
-  add_foreign_key 'club_students', 'students'
-  add_foreign_key 'executives', 'club_students'
-  add_foreign_key 'patrons', 'club_staffs'
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "club_staffs", "clubs"
+  add_foreign_key "club_staffs", "staffs"
+  add_foreign_key "club_students", "clubs"
+  add_foreign_key "club_students", "students"
+  add_foreign_key "executives", "club_students"
+  add_foreign_key "patrons", "club_staffs"
 end
