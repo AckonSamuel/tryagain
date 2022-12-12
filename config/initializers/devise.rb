6c33685628be9b7ca9c8f92b27c989ceb2eae8c6
Ackon Samuel <ackons433@gmail.com>
2022-12-12 09:58:35 +0000

    Configure devise-jwt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,4 +310,15 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.navigational_formats = []
+
+  config.jwt do |jwt|
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}],
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 5.minutes.to_i
+  end
 end
