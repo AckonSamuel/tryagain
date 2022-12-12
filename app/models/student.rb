cf684abe120acd663225a14e42168b75d5960bb3
Ackon Samuel <ackons433@gmail.com>
2022-12-12 10:08:04 +0000

    Update student model to use jwt token

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,8 @@ class Student < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   has_many :club_student
 end
