2ed3f0b9e214eb426874cb8aed0af08cfe706c7a
Ackon Samuel <ackons433@gmail.com>
2022-12-13 14:55:04 +0000

    confiure student registrations controller

--- app/controllers/students/sessions_controller.rb
+++ app/controllers/students/sessions_controller.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
 class Students::SessionsController < Devise::SessionsController
-respond_to :json
-private
+  # before_action :configure_sign_in_params, only: [:create]
 
-def respond_with(resource, opts = {})
-  render json: resource
-end
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
-def respond_to_on_destroy
-  render json: { message: 'Logged out.'}
-end
-end
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
 
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
