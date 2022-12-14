690d695491b5da6104786d9126deea490000dcb8
Ackon Samuel <ackons433@gmail.com>
2022-12-14 14:00:49 +0000

    Migrate files

---
+++ db/schema.rb
@@ -0,0 +1,102 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_141223) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "club_staffs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "club_id", null: false
+    t.uuid "staff_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_id"], name: "index_club_staffs_on_club_id"
+    t.index ["staff_id"], name: "index_club_staffs_on_staff_id"
+  end
+
+  create_table "club_students", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "club_id", null: false
+    t.uuid "student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_id"], name: "index_club_students_on_club_id"
+    t.index ["student_id"], name: "index_club_students_on_student_id"
+  end
+
+  create_table "clubs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
+    t.integer "telephone_number", null: false
+    t.string "group", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_clubs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_clubs_on_reset_password_token", unique: true
+  end
+
+  create_table "executives", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "portfolio"
+    t.uuid "club_student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_student_id"], name: "index_executives_on_club_student_id"
+  end
+
+  create_table "patrons", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "club_staff_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_staff_id"], name: "index_patrons_on_club_staff_id"
+  end
+
+  create_table "staffs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
+    t.integer "phone_number", null: false
+    t.string "role", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_staffs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
+  end
+
+  create_table "students", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
+    t.integer "phone_number", null: false
+    t.string "programme_name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "club_staffs", "clubs"
+  add_foreign_key "club_staffs", "staffs"
+  add_foreign_key "club_students", "clubs"
+  add_foreign_key "club_students", "students"
+  add_foreign_key "executives", "club_students"
+  add_foreign_key "patrons", "club_staffs"
+end
