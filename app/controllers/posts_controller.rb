74983d0ec7954b8a0cbdbd46744ba8f4df9ae6c6
Ackon Samuel <ackons433@gmail.com>
2022-12-31 20:27:10 +0000

    post validation and controller strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:caption)
+      params.require(:post).permit(:caption, :image)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    has_one_attached :image
+
+    validates :caption, :image, presence: true
+
+    def imageUrl
+        Rails.application.routes.url_helpers.url_for(image) if image.attached?
+        end
+    end
 end
