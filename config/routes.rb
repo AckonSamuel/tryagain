787edc57f5a0895a7521392e892c2dbe4530f2bb
Ackon Samuel <ackons433@gmail.com>
2022-12-31 20:06:15 +0000

    Generate scaffold resources for post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :posts
   # devise_for :staffs
   # devise_for :students
   # devise_for :clubs
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_113030) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_200420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -65,6 +65,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_113030) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["club_staff_id"], name: "index_patrons_on_club_staff_id"
+  end
+
+  create_table "posts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "staffs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
