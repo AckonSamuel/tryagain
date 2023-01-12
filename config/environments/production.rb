d3a0aafa94d03dffe6498f2995cdfc7d32950743
Ackon Samuel <ackons433@gmail.com>
2023-01-12 10:08:43 +0000

    Configure render host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
