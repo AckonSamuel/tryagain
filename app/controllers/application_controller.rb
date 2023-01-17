621e83df1c1d074de2dd483fc9ba4c4b9e712984
Ackon Samuel <ackons433@gmail.com>
2023-01-17 18:11:34 +0000

    Override default_url in application controller actually

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::API
     include ActionController::MimeResponds
+
+  def default_url_options
+    {
+      host: 'tryagainclub.s3.amazonaws.com',
+      protocol: 'https'
+    }
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,11 +2,4 @@
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-
-  def default_url_options
-    {
-      host: 'tryagainclub.s3.amazonaws.com',
-      protocol: 'https'
-    }
-  end
 end
