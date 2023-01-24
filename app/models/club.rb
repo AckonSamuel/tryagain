d14babdb0e4bc9505dc0878bde372e51628c6533
Ackon Samuel <ackons433@gmail.com>
2023-01-24 14:25:15 +0000

    Refactor attachment syntax again

--- app/models/club.rb
+++ app/models/club.rb
@@ -46,7 +46,6 @@ class Club < ApplicationRecord
 
   def passport_photos_url
     return [] unless self.passport_photos.attached?
-    self.passport_photos.map(&:attachment).map(&:url)
-
+    self.passport_photos.attachment.url
   end
 end
