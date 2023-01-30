46800b16ceaa3c9d0f8b32d16f1f00331987ecd6
Ackon Samuel <ackons433@gmail.com>
2023-01-30 09:37:28 +0000

    Replace verification with confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
                                                                          sign_in: 'login',
                                                                          sign_out: 'logout',
                                                                          registration: 'signup',
-                                                                         confirmation: 'verification'
                                                                        },
                      controllers: {
                        sessions: 'clubs/sessions',
