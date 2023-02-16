00146c04d13e3afa09a2d67c01e44f6a0645867e
Ackon Samuel <ackons433@gmail.com>
2023-02-16 11:06:38 +0000

    Put contents of respond_with method into create method

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -10,7 +10,11 @@ module Admins
         if resource.confirmed?
           sign_in(resource_name, resource)
           yield resource if block_given?
-          respond_with resource, location: after_sign_in_path_for(resource)
+          render json: {
+            status: { code: 200, message: 'Logged in sucessfully.' },
+            data: resource,
+            accessToken: current_token
+          }, status: :ok
         else
           set_flash_message!(:error, :unconfirmed_email)
           respond_with({ error: 'Please confirm your email address before signing in' }, status: :unprocessable_entity)
